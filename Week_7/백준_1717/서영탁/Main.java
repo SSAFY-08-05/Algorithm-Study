d8e50deb5929e574f3f43cf47043ed6b870e0961
0takkk <ss0takk@gmail.com>
2022-09-12 23:37:43 +0900

    Create Main.java

---
+++ Week_7/백준_1717/서영탁/Main.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n+1];
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(m-->0){
+            st = new StringTokenizer(br.readLine());
+            int comd = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(comd == 0) union(a, b);
+            else{
+                if(find(a) != find(b)) sb.append("NO\n");
+                else sb.append("YES\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static int find(int x){
+        if(x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y){
+        x = find(x);
+        y = find(y);
+
+        if(x != y) parent[y] = x;
+    }
+
+}
